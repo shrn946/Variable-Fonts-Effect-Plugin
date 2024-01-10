f3e2b674553608d25e341fe5a1a310dc1107ed26
shrn946 <114462856+shrn946@users.noreply.github.com>
2024-01-10 15:48:14 +0500

    Variable Fonts Effect Plugin
     Variable Fonts Effect To Wordpress Elementor

---
+++ readme.txt
@@ -0,0 +1,7 @@
+The Variable Fonts Effect Plugin is a powerful tool designed to enhance your WordPress site by adding captivating text effects. Easily elevate the visual appeal of your content with customizable variable fonts.
+
+Variable Fonts Effect Plugin is released under the GNU General Public License v2 or later.
+To apply the Variable Fonts Effect to your content, use the following shortcode:
+
+
+[custom_text text="Hello World"]
---
+++ variable-fonts.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: Variable Fonts Effect WordPress
+Description: Add a text effect to your site. [custom_text text="Hello World"]
+
+Version: 1.0
+Author: Hasan Naqvi
+*/
+
+// Enqueue scripts and styles
+function text_effect_enqueue_scripts() {
+    // Enqueue Dat.GUI library
+    wp_enqueue_script('dat-gui', 'https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.7.5/dat.gui.min.js', array(), '0.7.5', true);
+
+    // Enqueue your custom script
+    wp_enqueue_script('text-effect-script', plugin_dir_url(__FILE__) . 'script.js', array('dat-gui'), '1.0', true);
+
+    // Enqueue your custom styles
+    wp_enqueue_style('text-effect-style', plugin_dir_url(__FILE__) . 'style.css', array(), '1.0');
+}
+add_action('wp_enqueue_scripts', 'text_effect_enqueue_scripts');
+
+function custom_text_shortcode($atts) {
+    // Extract shortcode attributes
+    $atts = shortcode_atts(
+        array(
+            'text' => 'Custom Text',  // Default text
+        ),
+        $atts,
+        'custom_text'
+    );
+
+    // Sanitize shortcode attribute
+    $text = esc_html($atts['text']);
+
+    // Build the output HTML
+    $output = "<div id='fit'>";
+    $output .= "<h1 id='title'>$text</h1>";
+    $output .= "</div>";
+
+    return $output;
+}
+
+// Shortcode with text change option
+add_shortcode('custom_text', 'custom_text_shortcode');
